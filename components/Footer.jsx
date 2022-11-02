e0c99d5b31ef67b603f2ea93356b3ec38d4f4a65
AdeshDaniel <stockhutfxcompany@gmail.com>
2022-11-02 06:07:50 +0100

    continues

---
+++ components/Footer.jsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => (
+  <Box textAlign="center" p="5" color="grey.600">
+    2022 Realtor Inc
+  </Box>
+);
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
       />
-      <Flex>
+      <Flex flexWrap="wrap">
         {propertiesForSale.map((property) => (
           <Property property={property} key={property.id} />
         ))}
